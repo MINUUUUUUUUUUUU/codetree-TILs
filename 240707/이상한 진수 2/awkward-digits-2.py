c71acdf16eadc2317d419330b410f37a3ed074a0
KWON-MINWOO <117968404+MINUUUUUUUUUUUU@users.noreply.github.com>
2024-07-07 16:36:29 +0900

    Title: 이상한 진수 2, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240707/이상한 진수 2/awkward-digits-2.py
+++ 240707/이상한 진수 2/awkward-digits-2.py
@@ -4,6 +4,8 @@ for i in range(0,len(n)):
     if int(n[i]) == 0:
         n = n[:i] + '1' + n[i+1:]
         break
+    if i == (len(n) - 1):
+        n = n[:i] + '0'
 
 result = int(n, 2)
 print(result)
